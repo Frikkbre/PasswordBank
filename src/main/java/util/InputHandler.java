b5d60870d0622c85110dfdb8018392938c1d8a01
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-30 00:35:36 +0100

    Added methods inputString and inputInt to inputHandler class

--- src/main/java/util/InputHandler.java
+++ src/main/java/util/InputHandler.java
@@ -1,4 +1,17 @@
 package util;
 
+import java.util.Scanner;
+
 public class InputHandler {
+  Scanner inputScanner = new Scanner(System.in);
+
+  public String inputString() {
+    return inputScanner.nextLine();
+}
+
+  public int inputInt() {
+    //inputScanner.nextLine(); TODO - Does this need nextLine to not skip next input?
+    return inputScanner.nextInt();
+
+  }
 }
