8a6c738919f411cf3a565a2429af2d713766746c
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-30 00:31:13 +0100

    Added printString and printInt method to PrintHandler

--- src/main/java/util/PrintHandler.java
+++ src/main/java/util/PrintHandler.java
@@ -1,4 +1,12 @@
 package util;
 
 public class PrintHandler {
+
+  public void printString(String stringToPrint){
+    System.out.println(stringToPrint);
+  }
+
+  public void printInt(int intToPrint){
+    System.out.println(intToPrint);
+  }
 }
