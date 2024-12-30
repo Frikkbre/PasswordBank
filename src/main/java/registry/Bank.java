6d115655a677933b0502b9b25fb8c823d4857cbf
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-30 01:24:25 +0100

    Added HashMap register, addPassword and removePassword

--- src/main/java/registry/Bank.java
+++ src/main/java/registry/Bank.java
@@ -1,3 +1,12 @@
+package registry;
+
+import java.util.HashMap;
+
+import entity.Password;
+import util.InputHandler;
+import util.PrintHandler;
+
+
 /**
  * This class acts as a register for storing passwords
  *
@@ -5,4 +14,34 @@
  * @author Frikk Brændsrød
  */
 public class Bank {
+  private HashMap<String, Password> passwords;
+
+  public Bank() {
+    passwords = new HashMap<String, Password>();
+  }
+
+  public void addPassword() {
+    String userName = InputHandler.inputSting();
+    String password = InputHandler.inputString();
+    String confirmPassword = InputHandler.inputString();
+    String platform = InputHandler.inputString();
+
+    Password newPassword = new Password(userName, password, confirmPassword, platform);
+    passwords.put(newPassword.getPlatform(), newPassword);
+
+  }
+
+  public void removePassword() {
+    String platform = InputHandler.inputString();
+    if (passwords.containsKey(platform)) {
+      passwords.remove(platform);
+    } else{
+      PrintHandler.printString("There is no password for this platform");
+  }
+  }
+
+
+
+
+
 }
