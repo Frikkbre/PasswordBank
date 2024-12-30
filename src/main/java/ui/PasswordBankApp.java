bdde203652643a510aa06b3c8205879c5b578382
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-30 01:28:06 +0100

    Added comments to app class

--- src/main/java/ui/PasswordBankApp.java
+++ src/main/java/ui/PasswordBankApp.java
@@ -1,4 +1,7 @@
 package ui;
-
+/*
+This class should use switch case and printhandler, inputHandler to display a TUI
+From here you shoudl be able to call the different methods of bank and other classes.
+ */
 public class PasswordBankApp {
 }
