772a6df31f73a4edd557f5c1bba2528fb3736ee2
Frikk Brændsrød <fribra05@yahoo.com>
2024-12-31 14:55:38 +0100

    Added construtor and psvm til Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import ui.PasswordBankApp;
+
 public class Main {
-  public void Main{
-    //TODO - Make new instance og UI class
+  public Main(){
+    PasswordBankApp app = new PasswordBankApp();
   }
   public static void main(String[] args) {
     Main program = new Main();
